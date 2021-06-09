0dfbb687a7b9d9cafaf331532b8247a936aa508d
Quentin (admin) <quentin.bardenet@gmail.com>
2021-06-09 11:50:41 +0200

    Basic JWT authentication with express

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,9 @@
-const { find, create } = require("../models/users");
+const { find, create, findByEmail } = require("../models/users");
+const {
+  hashPassword,
+  validatePassword,
+} = require("../services/password.services");
+const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res) => {
   const [data] = await find();
@@ -7,11 +12,41 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    await create(req.body);
+    await create({
+      ...req.body,
+      password: await hashPassword(req.body.password),
+    });
     res.status(200).send("ok");
   } catch (error) {
-    res.status(500).send("Error");
+    res.status(500).send(error);
   }
 };
 
-module.exports = { getUsers, createUser };
+const loginUser = async (req, res) => {
+  const [user] = await findByEmail(req.body?.email);
+  if (!user) {
+    res.status(401).send("Unauthorized");
+  }
+
+  if (!validatePassword(req.body?.password, user[0].password)) {
+    res.status(401).send("Unauthorized");
+  }
+
+  const payload = {
+    createdAt: new Date().toISOString(),
+    user: user[0],
+  };
+
+  res.json({
+    access_token: jwt.sign(payload, process.env.JWT_SECRET, {
+      algorithm: "HS256",
+      expiresIn: 120,
+    }),
+  });
+};
+
+const getProfile = async (req, res) => {
+  res.json(req.user);
+};
+
+module.exports = { getUsers, createUser, loginUser, getProfile };
--- models/users.js
+++ models/users.js
@@ -4,13 +4,17 @@ const find = async () => {
   return db.promise().query("select * from Users");
 };
 
-const create = async ({ firstname, lastname }) => {
+const create = async ({ firstname, lastname, email, password }) => {
   return db
     .promise()
-    .query("INSERT INTO Users (firstname, lastname) VALUES (?, ?)", [
-      firstname,
-      lastname,
-    ]);
+    .query(
+      "INSERT INTO Users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)",
+      [firstname, lastname, email, password]
+    );
 };
 
-module.exports = { find, create };
+const findByEmail = async (email) => {
+  return db.promise().query("SELECT * FROM Users WHERE email = ?", [email]);
+};
+
+module.exports = { find, create, findByEmail };
